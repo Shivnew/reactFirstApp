dd6a961a56a29f74b4f0a8db0c61381dbab78a37
Shivnew <contactshivamm@gmail.com>
2025-12-31 19:40:02 +0530

    My third commit- parent child props logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Parent from './parentChild/Parent'
+import Parent1 from './PrentChildProps/Parent1'
 
 function App() {
 
   return (
     <>
-      <Parent />
+      <Parent1 />
     </>
   )
 }
